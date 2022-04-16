0175adb027b4d7bb95d19e7cb6078199c0f3a1ad
Arne Rief <arne-rief_student2022@wilder.school>
2022-04-16 22:52:54 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import QuoteCard from './QuoteCard';
+import React, { useState } from 'react';
 
 function App() {
+  let [quote, setQuote] = useState(
+    {
+    quote: 'Oh, so they have Internet on computers now!',
+    character: 'Homer Simpson',
+    image:
+      'https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939',
+    characterDirection: 'Right',
+    }
+  );
+
+  const url = "https://simpsons-quotes-api.herokuapp.com/quotes";
+
+  const simpsonQuote = () => {  
+    axios.get(url)
+    .then((response) => response.data)
+    .then((data) => setQuote(data[0]));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick={simpsonQuote}>Get a new quote</button>
+        <QuoteCard {... quote} />
     </div>
   );
 }
